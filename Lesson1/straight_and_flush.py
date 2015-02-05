8dd6407ff8d6fe509426996ce2688378f44a2acb
dsdshcym <dsdshcym@gmail.com>
2015-02-05 09:38:49 +0800

    Complete Straight and Flush

---
+++ Lesson1/straight_and_flush.py
@@ -0,0 +1,42 @@
+# -----------
+# User Instructions
+#
+# Define two functions, straight(ranks) and flush(hand).
+# Keep in mind that ranks will be ordered from largest
+# to smallest.
+
+def straight(ranks):
+    "Return True if the ordered ranks form a 5-card straight."
+    # Suggest solution
+    return (max(ranks) - min(ranks) == 4) and (len(set(ranks)) == 5)
+
+    # My solution
+    for i in range(1, len(ranks)):
+        if ranks[i] != ranks[i-1] - 1:
+            return False
+    return True
+
+def flush(hand):
+    "Return True if all the cards have the same suit."
+    # Suggest solution
+    suits = [s for r, s in hand]
+    return len(set(suits)) == 1
+
+    # My solution
+    for i in range(1, len(hand)):
+        if hand[i][1] != hand[i-1][1]:
+            return False
+    return True
+
+def test():
+    "Test cases for the functions in poker program."
+    sf = "6C 7C 8C 9C TC".split()
+    fk = "9D 9H 9S 9C 7D".split()
+    fh = "TD TC TH 7C 7D".split()
+    assert straight([9, 8, 7, 6, 5]) == True
+    assert straight([9, 8, 8, 6, 5]) == False
+    assert flush(sf) == True
+    assert flush(fk) == False
+    return 'tests pass'
+
+print test()
