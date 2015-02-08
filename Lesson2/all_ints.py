144e411a3b77a862127840c817423020c7ab5407
dsdshcym <dsdshcym@gmail.com>
2015-02-08 21:29:17 +0800

    Complete All ints

---
+++ Lesson2/all_ints.py
@@ -0,0 +1,29 @@
+# ------------
+# User Instructions
+#
+# Define a function, all_ints(), that generates the
+# integers in the order 0, +1, -1, +2, -2, ...
+
+def ints(start, end = None):
+    i = start
+    while i <= end or end is None:
+        yield i
+        i = i + 1
+
+
+def all_ints():
+    "Generate integers in the order 0, +1, -1, +2, -2, +3, -3, ..."
+    yield 0
+    i = 1
+    # Suggest solution
+    while True:
+        yield +i
+        yield -i
+        i += 1
+    # My solution
+    i = 0
+    while True:
+        yield i
+        i = -i + 1
+        yield i
+        i = -i
