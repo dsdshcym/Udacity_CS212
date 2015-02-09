a15678f43521b0e35402a0cf9a39fa16fc25ff6e
dsdshcym <dsdshcym@gmail.com>
2015-02-09 09:59:13 +0800

    Complete Translation Tables

---
+++ Lesson2/translation_tables.py
@@ -0,0 +1,8 @@
+import string, re
+
+def valid(f):
+    "Formula f is valid iff it has no numbers with leading zero and evals true."
+    try:
+        return not re.search(r'\b0[0-9]', f) and eval(f) is True
+    except ZeroDivisionError:
+        return False
